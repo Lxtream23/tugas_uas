6c679ca16e4f6c2046e0033a99ceffbaa3c0e169
lukman <lh23072002@gmail.com>
2025-05-20 11:21:47 +0700

    Fitur Dark Mode Toggle (Fix Error)

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,16 @@ import 'package:tugas_uas/pages/profile_form_page.dart';
 import 'package:tugas_uas/pages/register_page.dart';
 import 'package:tugas_uas/pages/splash_page.dart';
 import 'package:tugas_uas/pages/settings_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  await Supabase.initialize(
+    url: 'https://pkdoslagpkuxbstpuvil.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrZG9zbGFncGt1eGJzdHB1dmlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxNDc0NDgsImV4cCI6MjA2MTcyMzQ0OH0.4pZTEO6DM_CD8-7bap2VEgS5dVRpnxPsgn3gdq2rZYQ',
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -17,39 +27,62 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   ThemeMode _themeMode = ThemeMode.system;
 
-  void main() async {
-    await Supabase.initialize(
-      url: 'https://pkdoslagpkuxbstpuvil.supabase.co',
-      anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrZG9zbGFncGt1eGJzdHB1dmlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxNDc0NDgsImV4cCI6MjA2MTcyMzQ0OH0.4pZTEO6DM_CD8-7bap2VEgS5dVRpnxPsgn3gdq2rZYQ',
-    );
-    runApp(MyApp());
+  @override
+  void initState() {
+    super.initState();
+    _loadThemeMode();
+  }
+
+  void _onThemeChanged(ThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('theme_mode', mode.toString());
+    setState(() {
+      _themeMode = mode;
+    });
+  }
+
+  Future<void> _loadThemeMode() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final mode = prefs.getString('theme_mode');
+      setState(() {
+        if (mode == 'ThemeMode.dark') {
+          _themeMode = ThemeMode.dark;
+        } else if (mode == 'ThemeMode.light') {
+          _themeMode = ThemeMode.light;
+        } else {
+          _themeMode = ThemeMode.system;
+        }
+      });
+    } catch (e) {
+      print('❌ Error: $e');
+      setState(() {
+        _themeMode = ThemeMode.system;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Catatan Harian',
-      theme: ThemeData.light(), // Tema terang
-      darkTheme: ThemeData.dark(), // Tema gelap
-      themeMode: _themeMode, // 🔁 Gunakan ini
-      home: HomePage(
-        onThemeChanged: (mode) {
-          setState(() {
-            _themeMode = mode;
-          });
-        },
-      ),
-
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode,
       initialRoute: '/',
       routes: {
         '/': (_) => const SplashPage(),
         '/login': (_) => const LoginPage(),
         '/register': (_) => const RegisterPage(),
-        '/home': (_) => const SessionGuard(child: HomePage()),
-        '/detail': (context) => const SessionGuard(child: DetailPage()),
-        '/settings': (context) => const SessionGuard(child: SettingsPage()),
+        '/home':
+            (_) =>
+                SessionGuard(child: HomePage(onThemeChanged: _onThemeChanged)),
+        '/detail': (_) => const SessionGuard(child: DetailPage()),
+        '/settings':
+            (_) => SessionGuard(
+              child: SettingsPage(onThemeChanged: _onThemeChanged),
+            ),
         '/profile-form': (_) => const SessionGuard(child: ProfileFormPage()),
       },
     );
